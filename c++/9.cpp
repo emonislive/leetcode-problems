a74cedaabeb83da4c7e00ad16674f3a72ef74f37
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-10 21:35:45 +0600

    Palindrome Number

--- c++/9.cpp
+++ c++/9.cpp
@@ -1,3 +1,10 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 10-07-24
+*/
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -7,13 +14,12 @@ public:
 
         while (a > 0){
             mod = a % 10;           // gets the last num
-            rev = rev * 10 + mod;   // reversing the num
+            rev = rev * 10 + mod;   // reverse adding the num
             a = a / 10;             // gets the next last num
         }
         if (x == rev)
             return true;
         else
-            return false;
-        
+            return false;       
     }
 };
