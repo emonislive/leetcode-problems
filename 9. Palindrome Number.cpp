baf7362ee1b03cadac01176d4c7972a2b50bd842
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-03 04:18:02 +0600

    Topic: Math

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        string revNum = num;
+        
+        reverse(revNum.begin(), revNum.end());
+      
+        if (num == revNum)
+            return true;
+        else 
+            return false;
+    }
+};
