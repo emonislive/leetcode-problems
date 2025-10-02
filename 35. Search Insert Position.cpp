f118454da061b5db36b9ed4acb829e1b37261ca9
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-03 02:36:11 +0600

    Topic: Array, Binary search

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                index = i;
+                break;
+            }
+            if (nums[i] > target) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1)
+            return nums.size();
+
+        return index;
+    }
+};
