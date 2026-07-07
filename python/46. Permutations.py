bf5df33c2eb809b995c182645a732baec09f5781
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2026-07-07 19:33:16 +0600

    Array, Backtracking

--- python/46. Permutations.py
+++ python/46. Permutations.py
@@ -1,5 +1,6 @@
 # Time complexity: O(n * n!)
-# Space complexity: O(n * n!)
+# Space complexity: O(n)
+# Method 1: Backtracking with Temporary Path
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         temp, answer = [], []
@@ -18,4 +19,25 @@ class Solution:
         index = 0       
         backtrack(index)
         return answer
-    
+
+
+# Time complexity: O(n * n!)
+# Space complexity: O(n)
+# Method 2: Swap-based Backtracking (In-place)
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def backtrack(index):
+            if index == n - 1:
+                res.append(nums[:])
+                return
+
+            for i in range(index, n):
+                    nums[i], nums[index] = nums[index], nums[i]
+                    backtrack(index + 1)
+                    nums[index], nums[i] = nums[i], nums[index]
+
+        backtrack(0)
+        return res
